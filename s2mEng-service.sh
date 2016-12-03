2c49b2707fc17296f6ef07640fbde96f2a26968d
dfennell <dan.fennell.jr@gmail.com>
2016-12-03 16:43:33 -0500

    added startup shell script

--- s2mEng-service.sh
+++ s2mEng-service.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-forever --minUptime 30000 --spinSleepTime 10000 start ./forever/production.json
+forever -m 25 --minUptime 30000 --spinSleepTime 10000 start ./forever/production.json
